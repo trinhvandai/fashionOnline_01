2b0ade59744513d36dc359945d999a029d3583a6
CuTuanMinh <Caubeuudam@gmail.com>
2018-08-01 15:59:33 +0700

    setup database

--- database/migrations/2018_08_01_081056_create_products_table.php
+++ database/migrations/2018_08_01_081056_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('title')->nullable();
             $table->string('description')->nullable();
-            $table->string('image_url')->nullable();
-            $table->string('address');
-            $table->string('phone');
-            $table->string('role');
-            $table->rememberToken();
+            $table->float('price');
+            $table->string('status')->nullable();
+            $table->integer('brand_id')->unsigned();
+            $table->foreign('brand_id')
+                ->references('id')
+                ->on('brands')
+                ->onDelete('cascade');
+            $table->string('image_url');          
             $table->timestamps();
 
         });
@@ -36,6 +38,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
--- database/migrations/2018_08_01_081244_create_posts_table.php
+++ database/migrations/2018_08_01_081244_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('title');
             $table->string('description')->nullable();
-            $table->string('image_url')->nullable();
-            $table->string('address');
-            $table->string('phone');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('content');
+            $table->string('image_url');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
             $table->timestamps();
 
         });
@@ -36,6 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
