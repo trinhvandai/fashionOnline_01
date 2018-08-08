b956eda8c1aef8bd3e284235da1a27bd55d6dd7b
James Nguyen <frankierockie259@gmail.com>
2018-08-08 15:25:59 +0700

    @@

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,10 @@
                         @foreach ($errors->all() as $error)
                         <p class="alert alert-danger">{{ $error }}</p>
                         @endforeach
-                    {!! Form::text( 'name', null, ['class' => 'form-control' , 'placeholder' => __('Your name') ]); !!}
-                    {!! Form::email( 'email', null, ['class' => 'form-control' , 'placeholder' => __('Your email address') ]); !!}
-                    {!! Form::password( 'password', null, ['class' => 'form-control' , 'placeholder' => __('Password') ]); !!}
-                    {!! Form::password( 'password_confirmation', null, ['class' => 'form-control' , 'placeholder' => __('Confirm password') ]); !!}
+                    {!! Form::text( 'name', null, ['placeholder' => __('Your name') ]); !!}
+                    {!! Form::email( 'email', null, ['placeholder' => __('Your email address') ]); !!}
+                    {!! Form::password( 'password', null, ['placeholder' => __('Password') ]); !!}
+                    {!! Form::password( 'password_confirmation', null, ['placeholder' => __('Confirm password') ]); !!}
                     <!-- <input type="password" class="form-control" placeholder=" {{ __('Password') }} " name="password" />
                     <input type="password" class="form-control" placeholder=" {{ __('Confirm password') }} " name="password_confirmation" /> -->
                     {{ Form::button('Sign up', ['type' => 'submit', 'class' => 'btn btn-default'] )  }}
